051c3fd45f827d5c7a1d06c6137b07f35ef3c98d
james <jameslin809@gmail.com>
2026-02-22 19:27:41 -0500

    project 4 setup

--- Projects/Project3s/src/Main.java
+++ Projects/Project3s/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    private static String[] victor = {"Hi", "Bye"};
+    private static String[] wong = new String[5];
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for(int i = 0; i <victor.length; i++){
+            System.out.println(victor[i]);
+        }
+        for(int i = 0; i <wong.length; i++){
+            System.out.println(wong[i]);
+        }
     }
 }
